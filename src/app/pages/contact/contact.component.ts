611b5b69d272f5b03dc224c4465adcdc519ffe2b
Goico <tomasgoico7@gmail.com>
2024-01-19 19:51:23 -0300

    landing page

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, NgClass],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent implements OnInit{
+
+  contactForm! : FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    })
+  }
+
+  enviar(event : Event){
+    event.preventDefault();
+    console.log(this.contactForm.value)
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  hasErrors(field: string, typeError: string) {
+    return this.contactForm.get(field)?.hasError(typeError) && this.contactForm.get(field)?.touched
+  }
+
+}
